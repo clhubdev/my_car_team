cac04a2ddb5e42f0e9f7420a1376751d6a875bc0
Clément HUBERT <clhubertmail@gmail.com>
2025-02-09 16:46:54 +0100

    RouteMap ssr false

--- frontend/src/app/nouveau-trajet/page.js
+++ frontend/src/app/nouveau-trajet/page.js
@@ -1,8 +1,10 @@
 'use client';
 
+import dynamic from 'next/dynamic';
 import { useState } from "react";
 import AddressInput from "../components/AddressInput";
-import RouteMap from "../components/RouteMap";
+
+const RouteMap = dynamic(() => import('../components/RouteMap'), { ssr: false });
 
 export default function NewRoute() {
     const [itineraire, setItineraire] = useState({ start: null, end: null })
