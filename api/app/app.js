014282c676fa51f5d74859ad4f6128e517fe1789
Clément HUBERT <clhubertmail@gmail.com>
2025-02-09 16:10:30 +0100

    set cors rules

--- api/app/app.js
+++ api/app/app.js
@@ -1,8 +1,13 @@
 import express from 'express';
+import cors from 'cors';
 import routes from './routes/index.js';
-import { setupSwagger } from "../swagger.js"; 
+import { setupSwagger } from "../swagger.js";
 
 const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));
 
 // Middleware Swager (auto-doc api)
 setupSwagger(app);
