f6a254ca3dc3df762f6190c0e18ccb7a8fd3a4d8
Clément HUBERT <clhubertmail@gmail.com>
2025-02-15 15:14:21 +0100

    add setup tests before after for simulate BDD

--- api/src/config/jest.config.js
+++ api/src/config/jest.config.js
@@ -1,6 +1,7 @@
 export default {
     rootDir: '../../', 
     testEnvironment: 'node',
+    setupFilesAfterEnv: ['<rootDir>/tests/functionnal/setup.js'],
     testMatch: [
         '<rootDir>/tests/unit/*.test.js',
         '<rootDir>/tests/functionnal/*.test.js',
