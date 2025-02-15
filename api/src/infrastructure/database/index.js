258d80ba70ee2469c6965a9dfba52ffb9fbf58d2
Clément HUBERT <clhubertmail@gmail.com>
2025-02-15 15:02:19 +0100

    create databaseOptions for test

--- api/src/infrastructure/database/index.js
+++ api/src/infrastructure/database/index.js
@@ -1,12 +1,16 @@
 import Sequelize from 'sequelize';
 import config from '../../config/env.config.js';
+import testConfig from '../../config/database.test.js';
 
 
-const sequelize = new Sequelize(config.dbName, config.dbUser, config.dbPassword, {
+const databaseOptions = config.appEnv === 'test' ? testConfig : {
   host: config.dbHost,
   dialect: 'mysql',
   port: config.dbPort,
   logging: false, 
-});
+}
+
+
+const sequelize = new Sequelize(config.dbName, config.dbUser, config.dbPassword, databaseOptions);
 
 export default sequelize;
