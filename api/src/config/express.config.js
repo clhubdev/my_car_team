9b85c0d21ba9b06fdcbed3fdc732999c162100a5
Clément HUBERT <clhubertmail@gmail.com>
2025-02-23 00:46:19 +0100

    fix bug cors add log

--- api/src/config/express.config.js
+++ api/src/config/express.config.js
@@ -7,37 +7,32 @@ import config from './env.config.js';
 
 const app = express();
 
-// CORS Liste domaines autorisés
+// CORS : liste des domaines autorisés
 const whitelist = ['https://mycarteam.fr', config.frontendBaseURL];
 
+console.log()
+
 const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || process.env.NODE_ENV === 'test' || whitelist.indexOf(origin) !== -1) {
+  origin: (origin, callback) => {
+    if (!origin || process.env.NODE_ENV === 'test' || whitelist.includes(origin)) {
       callback(null, true);
     } else {
       console.error(`Origine non autorisée: ${origin}`);
       callback(new Error('Not allowed by CORS'));
     }
   },
-  credentials: true, 
+  credentials: true,
 };
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Credentials', 'true');
-  next();
-});
-
-app.options('*', cors(corsOptions));
+// Appliquer le middleware CORS pour toutes les routes
 app.use(cors(corsOptions));
-// End Cors
+app.options('*', cors(corsOptions));
 
-// Middleware Swager (auto-doc api)
+
 setupSwagger(app);
-
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use('/', routes);
 
 export default app;
