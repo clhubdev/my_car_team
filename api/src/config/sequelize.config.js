0ad4933d32c41016a8290ae5666a696e031ade40
Clément HUBERT <clhubertmail@gmail.com>
2025-02-22 20:55:16 +0100

    add production env in sequelize config

--- api/src/config/sequelize.config.js
+++ api/src/config/sequelize.config.js
@@ -7,5 +7,13 @@ export default {
     database: config.dbName,
     host: config.dbHost,
     dialect: 'mysql'
-  }
+  },
+
+  production: {
+    username: config.dbUser,
+    password: config.dbPassword,
+    database: config.dbName,
+    host: config.dbHost,
+    dialect: 'mysql'
+  },
 }
